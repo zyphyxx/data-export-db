0a6524d175ca887d57a2949405c27497f2b5561f
Edgar Mendes <mendesbr92@gmail.com>
2024-05-24 19:56:06 -0300

    rename py files

--- data-export-db/Sheets/Var.py
+++ data-export-db/Sheets/Var.py
@@ -3,5 +3,5 @@ class Path_Variables:
 
     sql_query = 'SELECT * FROM tb_py';
     db_url = 'mysql+pymysql://springstudent:springstudent@localhost:3306/exportdb';
-    file_path = '/Sheets\\import.txt'
+    file_path = '/sheets\\import.txt'
     LOCAL = 'C:\\Users\\ZyphyX\\OneDrive\\Desktop\\Devolucoes - São Paulo.csv'
