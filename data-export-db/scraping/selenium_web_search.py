64d7e353f715014c131ac9ecd90d67c8aa623e79
Edgar Mendes <mendesbr92@gmail.com>
2024-05-25 15:42:35 -0300

    working with selenium and webdriver

---
+++ data-export-db/scraping/selenium_web_search.py
@@ -0,0 +1,19 @@
+import pyautogui
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# set browser and get
+myBrowser = webdriver.Chrome()
+myBrowser.get('https://www.google.com')
+
+# search element and input command
+myBrowser.find_element(By.NAME, "q").send_keys("Dolar Hoje")
+# open dollar search
+myBrowser.find_element(By.NAME, "q").send_keys(Keys.RETURN)
+
+# xpath
+valueDollar = myBrowser.find_elements(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]')[0].text
+print('dolar:'+ valueDollar)
+
+pyautogui.sleep(5)
